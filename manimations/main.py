ed1495dc5bac15cb0146f5ef62950c3634fc483c
JaiJX <132177135+JaiJX@users.noreply.github.com>
2026-01-14 19:52:40 +0000

    ASdascd

--- manimations/main.py
+++ manimations/main.py
@@ -2,6 +2,10 @@ from manim import *
 
 class SimpleScene(Scene):
     def construct(self):
-       tx = Text("I hate Pyhsics")
+       tx = Text("A person who thinks all the time has nothing")
+       tx2 = Text("to think about except thoughts")
+       sq = Square(10, stroke_color=GREEN, fill_color=BLUE, fill_opacity=0.5)
        self.play(Write(tx))
+       self.play(Write(tx2.next_to(tx, DOWN)))
+       self.play(Create(sq))
        self.wait(2)
